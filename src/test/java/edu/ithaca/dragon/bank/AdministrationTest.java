83b872fe3fb52ed932499933eafea8141e1651e3
Ross Doane <rdoane@ithaca.edu>
2021-02-12 09:29:45 -0500

    added AdministrationTest class and methods

---
+++ src/test/java/edu/ithaca/dragon/bank/AdministrationTest.java
@@ -0,0 +1,23 @@
+package edu.ithaca.dragon.bank;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+public class AdministrationTest {
+    @Test
+    void getOverallMoneyTest(){
+
+    }
+    @Test
+    void requestReportTest(){
+
+    }
+    @Test
+    void freezeTest(){
+
+    }
+    @Test
+    void unfreezeTest(){
+
+    }
+}
