aa048c3ab8d4f8c9b5a8ba0e4cd93c314667e34b
Ross Doane <rdoane@ithaca.edu>
2021-02-14 23:00:25 -0500

    Created administration classes / method head

---
+++ src/main/java/edu/ithaca/dragon/bank/Administration.java
@@ -0,0 +1,17 @@
+package edu.ithaca.dragon.bank;
+
+import java.util.ArrayList;
+
+public class Administration {
+    private double equity;
+
+    /**
+     * Returns sum of all bank accounts
+     * @param accountList
+     * @return sum of all balances in accountList
+     */
+    public double getOverallMoney(ArrayList<BankAccount> accountList){
+        return 0.0;
+    }
+    
+}
--- src/test/java/edu/ithaca/dragon/bank/AdministrationTest.java
+++ src/test/java/edu/ithaca/dragon/bank/AdministrationTest.java
@@ -3,21 +3,42 @@ package edu.ithaca.dragon.bank;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
 public class AdministrationTest {
     @Test
     void getOverallMoneyTest(){
+    /** One equivalence class is the amount of balances being added together.
+     * One is the minimum, if it is less than one it will throw an exception. 
+     * There is no maximum. 
+     * Another equivalence class is correct/incorrect balances.
+     * An incorrect balance (negative) would throw an exception. Overall money should never
+     * be negative.
+     * Edge cases: only one bank account object added together, overall money = 0
+    */
+    
 
     }
     @Test
     void requestReportTest(){
+    /** Equivalence class: freezing non-suspicious accounts and freezing suspicious accounts
+     * Suspicious accounts would follow the following criteria: 
+     * 
+     */
 
     }
     @Test
     void freezeTest(){
+        /** Equivalence class:
+         * 
+         */
 
     }
     @Test
     void unfreezeTest(){
+        /** Equivalence class:
+         * 
+         */
 
     }
 }
