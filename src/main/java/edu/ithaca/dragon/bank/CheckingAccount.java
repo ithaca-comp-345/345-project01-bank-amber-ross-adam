d8fc67a763a87f12133184b37a911eb4d7997a2d
ADAM MARCISZEWSKI <AMARCISZEWSKI@ithaca.edu>
2021-02-15 09:15:46 -0500

    implemented CheckingAccount

---
+++ src/main/java/edu/ithaca/dragon/bank/CheckingAccount.java
@@ -0,0 +1,50 @@
+package edu.ithaca.dragon.bank;
+
+public class CheckingAccount implements BankAccount{
+
+    public String password;
+    public double balance;
+
+    public CheckingAccount(String passwordIn, double balanceIn){
+        this.password = passwordIn;
+        this.balance = balanceIn;
+    }
+
+    @Override
+    public double checkBal() {
+        return balance;
+    }
+
+    @Override
+    public void withdraw(double amount) throws InsufficientFundsException{
+        if ((amount <= balance) && (balance != 0)){
+            balance -= amount;
+        }
+        else{
+            throw new InsufficientFundsException("Not enough money in account");
+        }
+    }
+
+    @Override
+    public void deposit(double amount) {
+        balance += amount;
+    }
+
+    @Override
+    public void transferTo(BankAccount transferAccount, double amount) throws InsufficientFundsException {
+        if (this.balance <= amount){
+            this.balance -= amount;
+            transferAccount.deposit(amount);
+        }
+        else{
+            throw new InsufficientFundsException("Insufficient funds");
+        }
+    }
+
+    @Override
+    public String transactionHistory() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
