1f7a0e11f89ff8af202432ac63cc4bf4012191be
aelliott <aelliott@ithaca.edu>
2021-02-14 23:05:02 -0500

    BankAccountTeller Class

---
+++ src/main/java/edu/ithaca/dragon/bank/BankAccountTeller.java
@@ -0,0 +1,29 @@
+package edu.ithaca.dragon.bank;
+
+public class BankAccountTeller {
+    
+    private String accountID;
+    
+
+    /**
+     * @post Creates a new bankAccount() with accountID 
+     * returns the account number of this new account 
+     * Keep track of this accounts using array list.
+     * 
+     */
+    public int createAccount(String accountID) {
+        return 0;
+    }
+
+    /**
+     * @post remove accountID from array list
+     * what bankaccount you want to close in parm
+     */
+    public void closeAccount(String removeAccountID) {
+        
+    }   
+    
+    public void getAccountID() {
+        
+    }
+}
