1515534990b04c3a4aabe7c791d4a1770ab0ce8b
ADAM MARCISZEWSKI <AMARCISZEWSKI@ithaca.edu>
2021-02-24 20:29:25 -0500

    Updated test class for CheckingAccount

--- src/test/java/edu/ithaca/dragon/bank/CheckingAccountTest.java
+++ src/test/java/edu/ithaca/dragon/bank/CheckingAccountTest.java
@@ -7,9 +7,8 @@ import org.junit.jupiter.api.Test;
 public class CheckingAccountTest {
 
     @Test
-    public void checkingAccountTest() throws InsufficientFundsException {
+    public void withdrawTest() throws InsufficientFundsException {
         CheckingAccount myChecking = new CheckingAccount("carrot", 100);
-        CheckingAccount yourChecking = new CheckingAccount("zucchini", 100);
 
         assertEquals(myChecking.checkBal(), 100);
 
@@ -19,14 +18,30 @@ public class CheckingAccountTest {
         assertEquals(myChecking.checkBal(), 50);
         myChecking.withdraw(50);
         assertEquals(myChecking.checkBal(), 0);
+    }
 
+    @Test
+    public void depositTest(){
+        CheckingAccount myChecking = new CheckingAccount("carrot", 100);
         myChecking.deposit(100);
         assertEquals(myChecking.checkBal(), 100);
         myChecking.deposit(-100);
         assertEquals(myChecking.checkBal(), 100);
+    }
+
+    @Test
+    public void transferTest() throws InsufficientFundsException {
+        CheckingAccount myChecking = new CheckingAccount("carrot", 100);
+        CheckingAccount yourChecking = new CheckingAccount("zucchini", 100);
 
         myChecking.transferTo(yourChecking, 50);
-        
+        assertEquals(50, myChecking.checkBal());
+        assertEquals(150, yourChecking.checkBal());
+
+        yourChecking.transferTo(myChecking, 150);
+        assertEquals(200, myChecking.checkBal());
+        assertEquals(0, yourChecking.checkBal());
+
     }
     
 }
