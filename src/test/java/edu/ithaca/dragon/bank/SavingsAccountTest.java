4fa0a17c2f9e428aeb44d7547e0349de5939f6a2
ADAM MARCISZEWSKI <AMARCISZEWSKI@ithaca.edu>
2021-02-26 09:46:51 -0500

    created system test for SavingsAccount

--- src/test/java/edu/ithaca/dragon/bank/SavingsAccountTest.java
+++ src/test/java/edu/ithaca/dragon/bank/SavingsAccountTest.java
@@ -5,5 +5,23 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 public class SavingsAccountTest {
-    
+    @Test
+    public void savingsAccountSystemTest() throws InsufficientFundsException{
+        SavingsAccount mySavings = new SavingsAccount("apple", 100, 0.03, 500);
+
+        assertEquals("apple", mySavings.getPassword());
+
+        mySavings.deposit(100);
+        assertEquals(200, mySavings.checkBal());
+        mySavings.deposit(-100);
+        assertEquals(200, mySavings.checkBal());
+
+        mySavings.withdraw(100);
+        assertEquals(100, mySavings.checkBal());
+        mySavings.withdraw(101);
+        assertEquals(100, mySavings.checkBal());
+
+        mySavings.compound();
+        assertEquals(103, mySavings.checkBal());
+    }
 }
